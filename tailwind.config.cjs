ea2c3484ef0eff640f6ca2e07ad31eb39193ade5
Alex Flores_escritorio <alexander65.af@gmail.com>
2024-10-06 19:00:31 -0400

    [ADD] firts commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*/*.{astro,html,js,jsx}"],
+  theme: {
+    extend: {
+      animation: {
+        border: "background ease infinite",
+      },
+      keyframes: {
+        background: {
+          "0%, 100%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
