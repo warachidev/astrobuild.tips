5a729bfc0ef77a5e32916508ab5a09e5ecd0157f
Alex Flores_escritorio <alexander65.af@gmail.com>
2024-10-06 22:23:46 -0400

    Content y plantillas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   // ...
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
 });
---
+++ src/content/hello-javascript.md
@@ -0,0 +1,8 @@
+---
+title: "Hello JavaScript"
+pubDate: "2024-05-07"
+description: "Hello this is my first recommendation"
+image:
+tags: ["astro","javascript"]
+---
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aut qui
